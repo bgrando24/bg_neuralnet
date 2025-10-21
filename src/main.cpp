5e04853bf6fd8e5dfb654d3d6658243434b0884b
bgrando24 <bgrando24@gmail.com>
2025-10-21 12:54:00 +1100

    adding basic building blocks

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,45 @@
 #include <iostream>
 #include <spdlog/spdlog.h>
-#include <nlohmann/json.hpp>
+// spdlog::info("Hello from spdlog!");
+
+/**
+    * Basic building blocks
+       * Activation function + its derivative for back-propagation (struct?)
+       * Inputs: feature vector of numerical values
+       * Perceptrons: We will assume dense layers, receives M inputs, produces N outputs, weights can be a matrix of M x N size
+
+    * Basic structure:
+       * Input layer: can this just be the raw feature vector that feeds into the first hidden layer? No transformations are done on the input layer
+       * Hidden layer: array of N neurons, receives inputs from M number inputs from previous layer -> for each neuron in the next layer, provide output value + weight
+       * Connections: How to model these? Which neuron do the weights 'belong' to, the input or output?
+       * Output layer: Needs to output raw values, do we apply activation function here too? I think so
+*/
+
+// Defines the structure of a given activation function, provides both the base function and its derivative
+struct Activation_Function
+{
+    // std::function is any callable object (function/method)
+    // syntax is: <return-type(argument-type)>
+    std::function<double(double)> f;  // activation function itself
+    std::function<double(double)> df; // derivative of activation function for back-propagation
+};
+
+// Models a single perceptron unit
+class Perceptron
+{
+private:
+    double bias;
+    int input_connections;
+    int output_connections;
+
+public:
+    Perceptron(double start_bias, int input_connections, int output_connections)
+    {
+        }
+};
 
 int main()
 {
-    std::cout << "Hello, world!" << std::endl;
-    spdlog::info("Hello from spdlog!");
 
-    nlohmann::json j = {{"msg", "json works"}, {"n", 42}};
-    spdlog::info("JSON: {}", j.dump());
-
-    spdlog::info("All good!");
     return 0;
 }
